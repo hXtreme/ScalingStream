4fc26c83a60d0d85f53701053ba06f12b6499617
Harsh <h.x.dev@outlook.com>
2020-03-04 12:35:42 -0500

    Add tests to verify that Sink and Source complies and instantiates

--- src/test/java/org/example/scalingstream/operator/SinkTest.kt
+++ src/test/java/org/example/scalingstream/operator/SinkTest.kt
@@ -2,42 +2,37 @@ package org.example.scalingstream.operator
 
 import org.example.scalingstream.channels.*
 import org.example.scalingstream.partitioner.RoundRobinPartitioner
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.api.Assertions.*
 import java.time.Instant
 import java.util.*
 import kotlin.collections.HashMap
 
-internal class SourceTest {
+internal class SinkTest {
 
     companion object {
-        const val TEST_CLASS = "Test->SourceTest"
+        const val TEST_CLASS = "Test->SinkTest"
     }
 
     @Test
-    fun buildSource() {
+    fun buildSink() {
         val channelArgs = HashMap<ChannelArg, Any>()
         channelArgs[ChannelArg.LOCAL_QUEUE_DICT] = HashMap<String, Queue<Pair<Instant?, List<Any>?>>>()
         channelArgs[ChannelArg.MAX_QUEUE_LEN] = 2
 
-        val channelBuilder: ChannelBuilder = LocalChannelBuilder("${SinkTest.TEST_CLASS}->buildSource", channelArgs)
-        val s = Source<Any>(
+        val channelBuilder: ChannelBuilder = LocalChannelBuilder("$TEST_CLASS->buildSink", channelArgs)
+
+        val sink = Sink<Any>(
             0,
             "0",
-            listOf("1"),
+            listOf(),
             1,
             channelBuilder,
+            RoundRobinPartitioner(1),
             1,
-            RoundRobinPartitioner(2),
-            (fun(_): Any { return 1 })
+            fun(_): Unit {}
         )
 
         assertTrue(true)
-    }
-
-    @Test
-    fun run() {
-
     }
 }
