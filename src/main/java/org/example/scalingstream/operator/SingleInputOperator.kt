f3bb4f4488de76a7f4bc68f2f95be46b46d935f0
Harsh <h.x.dev@outlook.com>
2020-03-13 15:50:11 -0400

    Refactoring

--- src/main/java/org/example/scalingstream/operator/SingleInputOperator.kt
+++ src/main/java/org/example/scalingstream/operator/SingleInputOperator.kt
@@ -1,5 +1,6 @@
 package org.example.scalingstream.operator
 
+import de.jupf.staticlog.Log
 import org.example.scalingstream.CONSTANTS
 import org.example.scalingstream.channels.ChannelBuilder
 import org.example.scalingstream.partitioner.Partitioner
@@ -36,27 +37,35 @@ abstract class SingleInputOperator<InputType, FnInp, FnOut, OutputType>(
     // protected val state: HashMap<Any, Any> by lazy { HashMap<Any, Any>() }
 
     override fun run() {
-        var done = false
+        Log.info("Running $idx instance of $operatorID")
+        Log.info("$operatorID\t-->\t$operatorID\t-->\t${outOperatorIDs.joinToString(prefix="[", postfix="]" )}")
         inputChannel.connect()
         outputChannels.forEach { it.connect() }
+        var done = false
 
         while (!done) {
-            val (timestamp, recordsBatch) = inputChannel.get()
+            val (timestamp, recordBatch) = inputChannel.get()
             outputBuffers.timestamp = timestamp
-            if (recordsBatch == CONSTANTS.DONE_MARKER) {
+            if (recordBatch == CONSTANTS.DONE_MARKER) {
                 numDoneMarkers++
                 done = numDoneMarkers == upstreamCount
             } else {
-                processRecordBatch(recordsBatch)
-                numProcessed += recordsBatch.size
+                processRecordBatch(recordBatch)
+                numProcessed += recordBatch.size
             }
         }
+        Log.info("Processed $numProcessed records. Closing buffers and quitting $operatorID${idx}_")
+        outputBuffers.close()
     }
 
     override fun processRecordBatch(recordBatch: List<InputType>) {
-        partitioner.partitionBatch(
-            outputBuffers,
-            recordBatch.map { record -> processRecord(record) }
-        )
+            partitioner.partitionBatch(
+                outputBuffers,
+                recordBatch.map { record -> processRecord(record) }
+            ) // TODO("Want to handle this via channel manager")
+        /** TODO
+         * Want the following interface:
+         * channelManagers.forEach { it.put(recordBatch.map{record -> processRecord(record)}) }
+         */
     }
 }
