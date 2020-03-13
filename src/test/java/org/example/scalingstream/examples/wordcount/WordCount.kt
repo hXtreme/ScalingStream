9f35bea6b7e143aabe41623cf8bc0d709d034a16
Harsh <h.x.dev@outlook.com>
2020-03-13 15:55:01 -0400

    Larger tests + more smaller changes

--- src/test/java/org/example/scalingstream/examples/wordcount/WordCount.kt
+++ src/test/java/org/example/scalingstream/examples/wordcount/WordCount.kt
@@ -1,11 +1,14 @@
 package org.example.scalingstream.examples.wordcount
 
+import de.jupf.staticlog.Log
+import de.jupf.staticlog.core.LogLevel
 import org.example.scalingstream.StreamContext
 import org.example.scalingstream.channels.ChannelBuilder
 import org.example.scalingstream.channels.ChannelArg
 import org.example.scalingstream.channels.LocalChannelBuilder
 import org.example.scalingstream.executor.Executor
 import org.example.scalingstream.executor.LocalExecutor
+import org.example.scalingstream.partitioner.HashPartitioner
 import org.example.scalingstream.stream.Stream
 import java.io.File
 import java.time.Instant
@@ -14,7 +17,7 @@ import kotlin.collections.HashMap
 
 private const val NAME = "WordCount"
 
-class WordCount(private val sentences: SentenceSource) {
+class WordCount(private val sentences: SentenceSource, batchSize: Int = 4) {
     private val words: Stream<*, String?>
     private val context: StreamContext
 
@@ -22,13 +25,13 @@ class WordCount(private val sentences: SentenceSource) {
         val executor: Executor = LocalExecutor(NAME)
         val channelArgs = HashMap<ChannelArg, Any>()
         channelArgs[ChannelArg.LOCAL_QUEUE_DICT] = HashMap<String, Queue<Pair<Instant?, List<Any>?>>>()
-        channelArgs[ChannelArg.MAX_QUEUE_LEN] = 2
+        channelArgs[ChannelArg.MAX_QUEUE_LEN] = 10
 
         val channelBuilder: ChannelBuilder = LocalChannelBuilder(NAME, channelArgs)
-        context = StreamContext(executor, channelBuilder, channelArgs)
+        context = StreamContext(executor, channelBuilder, channelArgs, batchSize, ::HashPartitioner)
 
         words = context.createStream(NAME) { sentences.generator() }
-        words.print()
+//        words.print()
         val count = words.flatMap {
                 (it as String).split(Regex("\\s"))
                     .map { s -> Pair(s, 1) }
@@ -36,8 +39,10 @@ class WordCount(private val sentences: SentenceSource) {
             .keyBy { it.first }
             .reduce { (k, v1), (_, v2) -> Pair(k, v1 + v2) }
 
-        count.filter { (_, v) -> v > 50 }
-        count.print()
+//        count.filter { (_, v) -> v > 50 }//.drop()
+//        count.filter { (_, v) -> v > 50 }.print()
+//        count.print()
+//        count.drop()
     }
 
     fun run() {
@@ -47,8 +52,8 @@ class WordCount(private val sentences: SentenceSource) {
 
 fun main(args: Array<String>) {
     val file = File(args.getOrElse(0) { "./README.md" })
-    val sentenceSource = SentenceSource(file, 100, 10)
-    val wordCount = WordCount(sentenceSource)
-
+    val sentenceSource = SentenceSource(file, 10000, 100)
+    val wordCount = WordCount(sentenceSource, 50)
+//    Log.logLevel = LogLevel.ERROR
     wordCount.run()
 }
