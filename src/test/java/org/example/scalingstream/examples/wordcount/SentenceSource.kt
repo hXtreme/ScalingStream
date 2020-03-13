672e6847df0c8b39e8e810de2e008a1307bc1af8
Harsh <h.x.dev@outlook.com>
2020-03-13 15:38:52 -0400

    update SentenceSource

--- src/test/java/org/example/scalingstream/examples/wordcount/SentenceSource.kt
+++ src/test/java/org/example/scalingstream/examples/wordcount/SentenceSource.kt
@@ -1,5 +1,7 @@
 package org.example.scalingstream.examples.wordcount
 
+import de.jupf.staticlog.Log
+import de.jupf.staticlog.core.LogLevel
 import org.example.scalingstream.CONSTANTS
 import org.example.scalingstream.StreamContext
 import org.example.scalingstream.channels.ChannelArg
@@ -22,22 +24,23 @@ class SentenceSource(
     private val sentenceLength: Int,
     private val timestamp_interval: Duration? = null
 ) {
-    private val words: List<String> = file.readText().split(" ")
+    private val words: List<String> = file.readText().split(Regex("\\s"))
 
     private var num: Int = 0
 
     fun generateSentence(): String {
-        return (0..sentenceLength).joinToString(separator = " ") { words.random() }
+        return (0 until sentenceLength).joinToString(separator = " ") { words.random() }
     }
 
     fun generator(): String? {
-        return if (num++ < numRecords) generateSentence() else CONSTANTS.DONE_MARKER
+        return if (num++ < numRecords) generateSentence() else null
     }
 }
 
 fun main(args: Array<String>) {
+    Log.logLevel = LogLevel.ERROR
     val file = File(args.getOrElse(0) { "./README.md" })
-    val sentences = SentenceSource(file, 100, 10)
+    val sentences = SentenceSource(file, 20, 2)
 
     val executor: Executor = LocalExecutor(NAME)
     val channelArgs = HashMap<ChannelArg, Any>()
