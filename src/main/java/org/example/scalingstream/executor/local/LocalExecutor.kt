b2f74bf670a18d1d352096ea0ad57745b8f6da18
Harsh <h.x.dev@outlook.com>
2020-03-27 09:17:31 -0400

    Allow scaling down to a single task (from one task per operator)

--- src/main/java/org/example/scalingstream/executor/local/LocalExecutor.kt
+++ src/main/java/org/example/scalingstream/executor/local/LocalExecutor.kt
@@ -11,7 +11,7 @@ class LocalExecutor : Executor {
 
     override val type: String = "LOCAL"
 
-    private val runningTasks: MutableMap<Operator<*, *, *, *>, MutableList<Deployment>> = HashMap()
+    private val runningTasks: MutableSet<Pair<Operator<*, *, *, *>, Deployment>> = HashSet()
 
     override fun exec(dag: DirectedAcyclicGraph<Operator<*, *, *, *>, ChannelManager<*>>) {
 
@@ -19,21 +19,19 @@ class LocalExecutor : Executor {
             operators.run(deploy)
         }
 
-        runningTasks.forEach { (operator, deployments) ->
-            while (deployments.isNotEmpty()) {
-                deployments.forEach {
-                    Log.debug("Joining back ${it.name}", name)
-                    it.join()
-                }
-                operator.removeTasks(deployments.filter { it.isDone }.map { it.taskID })
-                deployments.removeIf { it.isDone }
+        while (runningTasks.isNotEmpty()) {
+            runningTasks.forEach { (operator, deployment) ->
+                Log.debug("Joining back ${deployment.name}", name)
+                deployment.join()
+                operator.removeTask(deployment.taskID)
             }
+            runningTasks.removeIf{ (_, deployment) -> deployment.isDone }
         }
     }
 
     override val deploy: DeployFn = fun(operator: Operator<*, *, *, *>, task: () -> Task<*, *, *, *>): Deployment {
         val deployment = LocalDeployment(task)
-        runningTasks.getOrPut(operator) { mutableListOf() }.add(deployment)
+        runningTasks.add(Pair(operator, deployment))
         return deployment
     }
 }
