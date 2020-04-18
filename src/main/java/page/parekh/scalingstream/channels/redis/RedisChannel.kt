bb518fde45a673d434c46878a37b4317217805db
Harsh <h.x.dev@outlook.com>
2020-04-18 17:17:37 -0400

    Implement Redis Channel

--- src/main/java/page/parekh/scalingstream/channels/redis/RedisChannel.kt
+++ src/main/java/page/parekh/scalingstream/channels/redis/RedisChannel.kt
@@ -1,12 +1,17 @@
 package page.parekh.scalingstream.channels.redis
 
 import page.parekh.scalingstream.channels.*
+import redis.clients.jedis.Jedis
 
-class RedisChannel<Type>(id: ChannelID, channelArgs: ChannelArgs) : AbstractChannel<Type>(id, channelArgs) {
-    override val type: String= "REDIS"
+class RedisChannel<Type>(
+    id: ChannelID,
+    channelArgs: ChannelArgs,
+    jedis: Jedis
+) : AbstractChannel<Type>(id, channelArgs) {
+    override val type: String = "REDIS"
 
     init {
-        TODO("Not yet implemented")
+        jedis.set("$id-status", "open")
     }
 
     override fun getChannelReader(): ChannelReader<Type> {
@@ -18,6 +23,6 @@ class RedisChannel<Type>(id: ChannelID, channelArgs: ChannelArgs) : AbstractChan
     }
 
     override fun destroy() {
-        TODO("Not yet implemented")
+        error("Should not have been invoked.")
     }
 }
