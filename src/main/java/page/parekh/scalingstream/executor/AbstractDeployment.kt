2c5dffdd72524de9da0617d21afdd8f51ac7d7f0
Harsh <h.x.dev@outlook.com>
2020-04-18 21:53:33 -0400

    Update deployment interface and add RPCDeployment

--- src/main/java/page/parekh/scalingstream/executor/AbstractDeployment.kt
+++ src/main/java/page/parekh/scalingstream/executor/AbstractDeployment.kt
@@ -1,8 +1,6 @@
 package page.parekh.scalingstream.executor
 
-import page.parekh.scalingstream.operator.Task
-
-abstract class AbstractDeployment(createTask: () -> Task<*, *, *, *>) : Deployment {
+abstract class AbstractDeployment : Deployment {
 
     override fun run() {
         error("Can't run an already deployed Task.")
