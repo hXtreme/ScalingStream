356735250587a3157e01cf1ee696a265399f1294
Harsh <h.x.dev@outlook.com>
2020-04-19 13:31:35 -0400

    Bug fix: negative HashPartition assignment

--- src/main/java/page/parekh/scalingstream/partitioner/HashPartitioner.kt
+++ src/main/java/page/parekh/scalingstream/partitioner/HashPartitioner.kt
@@ -1,11 +1,13 @@
 package page.parekh.scalingstream.partitioner
 
+import kotlin.math.abs
+
 
 class HashPartitioner : Partitioner {
     override val name: String = "HASH"
 
     override fun <Type> assignPartition(record: Type, numOut: Int): Int {
         if (numOut == 0) error("Can't partition into zero partitions.")
-        return record.hashCode() % numOut
+        return abs(record.hashCode() % numOut)
     }
 }
